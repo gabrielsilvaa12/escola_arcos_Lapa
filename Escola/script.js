41ecb76703d8a4da8643aaadc331d8c4166665c1
Gabriel Aparecido <gabrieusilvaa14@gmail.com>
2025-01-31 15:55:59 -0300

    Add files via upload

--- Escola/script.js
+++ Escola/script.js
@@ -1,19 +1,4 @@
-const carousel = document.querySelector(".carousel");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
- 
-let index = 0;
- 
-nextBtn.addEventListener("click", () => {
-    index = (index + 1) % 3; // Ajuste se adicionar mais imagens
-    updateCarousel();
+document.getElementById("search-button").addEventListener("click", function() {
+    let query = document.getElementById("search-bar").value;
+    alert("Você pesquisou por: " + query);
 });
- 
-prevBtn.addEventListener("click", () => {
-    index = (index - 1 + 3) % 3; // Ajuste se adicionar mais imagens
-    updateCarousel();
-});
- 
-function updateCarousel() {
-carousel.style.transform = `translateX(${-index * 100}%)`;
-}
